fb59349f954af9a2d9c32471e74c39e6fe36f019
Dmlehan <dmlehanamanda@gmail.com>
2026-01-17 19:15:25 +0530

    completed Header.js(header component) file

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <nav className="nav">
+      <img src={logo} alt="logo" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
