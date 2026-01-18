e8db8af04cf692bd8f8ee6c8876ff7739f9f9ba0
Dmlehan <dmlehanamanda@gmail.com>
2026-01-18 07:18:18 +0530

    added temporarily

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+ import logo from "./assets/logo.png";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+    {/* hijnsjkdnkfsnksfnksfksnfksnfksfnksnfjk */}
+      {/* <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Footer /> */}
+     
+ <img src={logo} alt="logo" className="logo" />
+          <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+    </>
+  );
+}
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+// import   "../App.css";
 import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <nav className="nav">
       <img src={logo} alt="logo" className="logo" />
+
       <ul>
         <li>Home</li>
         <li>About</li>
